c5138990508262e7956eb42ff7b81c33b623ae92
semvlu <74308718+semvlu@users.noreply.github.com>
2021-12-30 17:24:21 +0800

    Update checkspace.c

--- checkspace.c
+++ checkspace.c
@@ -4,8 +4,8 @@
 #include<ctype.h>
 #include<math.h>
 
-int checkspace(int a[],int m)
-{		//return 0 if ok    -1 if bad code
+int checkspace(int a[],int m)	//return 0 if ok, -1 if bad code
+{		
 	int i=5;
 	while(i<=m-6){
 		if(a[i]!=0){
