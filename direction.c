fbd6a865b532d4719d2f3fd085f65ddc2467e8b9
semvlu <74308718+semvlu@users.noreply.github.com>
2021-12-30 17:26:57 +0800

    Update direction.c

--- direction.c
+++ direction.c
@@ -4,14 +4,18 @@
 #include<ctype.h>
 #include<math.h>
 
-int direction(int a[],int m){		//return 1 if ok   0 if bad code
-	if(a[0]==0&&a[1]==0&&a[2]==1&&a[3]==1&&a[4]==0&&a[m-5]==0&&a[m-4]==0&&a[m-3]==1&&a[m-2]==1&&a[m-1]==0){		//upright
+int direction(int a[],int m)	//return 1 if ok, 0 if bad code
+{		
+	if(a[0]==0&&a[1]==0&&a[2]==1&&a[3]==1&&a[4]==0&&a[m-5]==0&&a[m-4]==0&&a[m-3]==1&&a[m-2]==1&&a[m-1]==0)
+	{	//upright
 		return 1;
 	}
-	else if(a[0]==0&&a[1]==1&&a[2]==1&&a[3]==0&&a[4]==0&&a[m-5]==0&&a[m-4]==1&&a[m-3]==1&&a[m-2]==0&&a[m-1]==0){		//invert
+	else if(a[0]==0&&a[1]==1&&a[2]==1&&a[3]==0&&a[4]==0&&a[m-5]==0&&a[m-4]==1&&a[m-3]==1&&a[m-2]==0&&a[m-1]==0)
+	{	//invert
 		int count=0;	
 		int hold;
-		while(count<(m-1)/2){		//reverse
+		while(count<(m-1)/2)
+		{	//reverse
 			hold=a[count];
 			a[count]=a[m-count-1];
 			a[m-count-1]=hold;
@@ -19,7 +23,8 @@ int direction(int a[],int m){		//return 1 if ok   0 if bad code
 		}
 		return 1;
 	}
-	else{
+	else
+	{
 		return 0;
 	}
 }
